608b121642cce437ec86db39ad28fff5692aac0c
TianqiLou <47435593+TianqiLou@users.noreply.github.com>
2021-12-09 00:37:36 -0500

    Add files via upload
    completed comments and documentations

--- ensemble_rf.py
+++ ensemble_rf.py
@@ -10,56 +10,58 @@ from scipy.signal import argrelextrema
 from sklearn.metrics import confusion_matrix
 
 class AutoRF():
-    
     '''
-    Vector Autoregression model
+    Auto Random Forest model
 
-    Attributes
-    ----------
-    self.data_backup: dataframe, a backup copy of the input dataset
-    self.data: dataframe, the main dataset worked on
-    self.n: int, length (unit in months) of target to predict
-    self.df_result: dataframe, stores the predicted target and the true target
-    self.lag: int, number of past months used to predict the target
-    self.rmse: rounded RMSE of the prediction
-    self.target: str, name of target variable
-    self.model: record a CNN trained model
-    self.train_result: record model fit result loss
-    
-    Params
-    ----------
-    data_name: str, name of the dataset. Notice the input dataset must contain a column named 'Date'
-    target_name: str, name of target variable
-    drop_cols: list of strings, names of columns to drop
+    Attributes:
+        self.data:         pandas.DataFrame --  the main dataset worked on
+        self.n:            int -- length (unit in months) of target to predict
+        self.target:       str -- name of target variable
+        self.n_features:   int -- num of features to use
+        self.truey:        list -- a list containing the true target variable
+        self.predX:        pandas.DataFrame -- a dataframe containing the predictors for the model
+        self.lags:         list -- each element in the list represents the num of months back used by the correspondin model
+        self.values_24:    list -- a list of length 24, containing the reshaped values of self.series_to_supervised
+        self.models:       list -- a list of lenght 24, containing models of different leads
+        self.train_result: list -- the fit() result of models of different leads
     '''
     
-    
-    
-    
-    def __init__(self, data_name, target_name):   
+    def __init__(self, data_name, target_name):  
+        '''
+        Initiate the class
+
+        Input:
+            data_name:   str -- name of the dataset. Notice the input dataset must contain a column named 'Date'
+            target_name: str --  name of target variable
+        '''
+
         #import data
         curr_path = os.getcwd()
         input_path = os.path.join(curr_path, data_name)
         data = pd.read_excel(input_path, index_col=0)
         
         #drop columns and na
-        #data.drop(drop_cols, axis=1, inplace=True)
         data.dropna(inplace = True)
-        # data.reset_index(drop=True, inplace=True)
         
         #set attributes
         self.data = data
- 
         self.n = 0
-        self.df_result = 0
-        self.lag = 0
         self.rmse = 0
         self.target = target_name
         self.n_features = len(data.columns) - 1
-        self.model = 0
-        self.train_result = 0
+
 
     def preprocess(self, dataset):
+        '''
+        Preprocess the training dataset. Add 'IsExpanding' column to the training dataset as the target variable 
+        for the model based on calculated local min and max.
+
+        Input:
+            dataset:   pandas.DataFrame -- training dataset
+
+        Output:
+            dataset:   pandas.DataFrame -- same dataset with an extra 'IsExpanding' colum
+        '''
         ilocs_min = argrelextrema(dataset['SP500-EPS-Index'].values, np.less_equal, mode = 'wrap', order = 12)[0]
         ilocs_max = argrelextrema(dataset['SP500-EPS-Index'].values, np.greater_equal, mode = 'wrap', order = 12)[0]
         
@@ -82,6 +84,19 @@ class AutoRF():
     
     
     def series_to_supervised(self, data, n_in=1, n_out=1, dropnan=True, if_target=True):
+        '''
+        Convert the dataset into a rolling window for the supervised learning.
+
+        Input:
+            data:      pandas.DataFrame -- training dataset
+            n_in:      int, default=1 -- number of month to be converted into one rolling window
+            n_out:     int, default=1 -- number of month in the future as the target variable for the rolling windows
+            dropnan:   bool, default=True -- whether to drop nan values in the rolling windows
+            if_target: bool, default=True -- whether to include target variable as a predictor in the rolling windows
+
+        Output:
+            agg:       pandas.DataFrame -- rolling windows of the same length concatenated into one single DataFrame
+        ''' 
         n_vars = 1 if type(data) is list else data.shape[1]
         df = pd.DataFrame(data)
         df_without_target = df.loc[:, df.columns[1:]]
@@ -114,7 +129,16 @@ class AutoRF():
             agg.dropna(inplace=True)
         return agg
     
+
     def get_pred_data(self, i, last_month):
+        '''
+        Helper function to store the true target variables and training data to the class
+
+        Input:
+            i:              int -- specifies which lag to use
+            last_month:     datetime64 -- the last month of the training data plus one month to retrieve 
+                                          the true target variable
+        '''
         index_num = self.data.index.get_loc(last_month)
 
         
@@ -126,7 +150,21 @@ class AutoRF():
         self.truey = reframed_predX.iloc[int(index_num.start)+1:int(index_num.start)+25, -1].values
         self.predX = reframed_predX.iloc[index_num,0:-1].values
     
+
     def get_predict(self, last_month, forward=24):
+        '''
+        Get the prediction result and the true target variable from the model
+
+        Input:
+            last_month:     datetime64 -- the last month of the training data plus one month to retrieve 
+                                          the true target variable
+            forward:        int, default=24 -- number of months in the future to predict
+
+        Output:
+            pred_y_list:    list -- a list containing the predicted target variable from the model
+            true_y_list:    list -- a list containing the true target variable from the training dataset
+        '''
+
         pred_y_list = []
         
         for i in range(forward):
@@ -134,10 +172,7 @@ class AutoRF():
             self.get_pred_data(i, last_month)
             model = self.models[i]
             
-
             test_X = self.predX
-            #true_y = self.truey
-            #true_y_list.append(true_y)
             
             # reshape input to be 3D [samples, timesteps, features]
             test_X = test_X.reshape((1, test_X.shape[1]))
@@ -150,6 +185,17 @@ class AutoRF():
     
 
     def get_backtesting(self):
+        '''
+        Specific funciton for backtesting purpose only
+
+        Input:
+            None
+
+        Output:
+            pred_y_list:    list -- a list containing the predicted target variable from the model
+            true_y_list:    list -- a list containing the true target variable from the training dataset
+        '''
+
         pred_y_list = []
         true_y_list = []
 
@@ -166,15 +212,14 @@ class AutoRF():
         return pred_y_list, true_y_list
 
 
-    def run(self, lags=[], leads=[]):
-        ''' Run CNN
+    def run(self, lags, leads):
+        ''' 
+        Run Random Forest
         
-        Params
-        ----------
-        pred_begin_date: date, the begin date of prediction
-        n: int, length of prediction (unit in months)      
-        lag: int, num of past months to consider
-        if_target: boolean, whether to include target as a predictor
+        Input:    
+            lags:        list -- each element in the list represents the num of months back used by the correspondin model                   
+            leads:       list -- the length of the list represents the num of months forward the model is trying to predict,
+                                 starting from 1 
         '''
 
         self.lags = lags
@@ -184,9 +229,7 @@ class AutoRF():
 
         n = 1
         
-        
         self.data = self.preprocess(self.data)
-        #print(self.data.columns)
         self.data.drop('SP500-EPS-Index', axis=1, inplace=True)
         self.data.dropna(inplace = True)
         
@@ -199,7 +242,6 @@ class AutoRF():
             # drop columns we don't want to predict
             reframed.drop(reframed.columns[range(reframed.shape[1] - self.n_features, reframed.shape[1])], axis=1, inplace=True)
             reframed.drop(reframed.columns[range(reframed.shape[1]-1-(self.n_features+1)*(lead-1), reframed.shape[1]-1)], axis=1, inplace=True)
-            #print(reframed.columns)
 
             values = reframed.values
             self.values_24.append(values)
@@ -209,10 +251,6 @@ class AutoRF():
 
             # split into input and outputs
             train_X, train_y = train[:, :-1], train[:, -1]
-            # reshape input to be 3D [samples, timesteps, features]
-            #train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-            #test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-            #print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
             # create and fit the LSTM network
             model = RandomForestClassifier(max_depth=5)
@@ -220,3 +258,7 @@ class AutoRF():
             result = model.fit(train_X, train_y)
             self.models.append(model)
             self.train_result = result
+
+
+
+
